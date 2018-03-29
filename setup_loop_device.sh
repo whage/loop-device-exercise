3965120aa8924e61b41720699951c349cbb742f8
whage <whage@freemail.hu>
2018-03-29 23:55:55 +0200

    replace single quotes with double quotes, remove AND_IF, for loop still broken. why?

--- setup_loop_device.sh
+++ setup_loop_device.sh
@@ -3,4 +3,5 @@ losetup /dev/loop0 back.file
 mkfs -t ext4 /dev/loop0
 mkdir mountpoint
 mount /dev/loop0 mountpoint
-cd mountpoint && for i in {1..100}; do touch 'file$i'; done
+cd mountpoint
+for i in {1..100}; do touch "file$i"; done
